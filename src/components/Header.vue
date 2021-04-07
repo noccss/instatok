d5d3998dcf22f9d81da8892727b03fcc4451be10
noccss <nicolas.ocarvalho@outlook.com>
2021-04-07 14:34:14 -0300

    Only the first configuration and layout of mobile on Figma

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav>
+      <h1>Logo</h1>
+
+      <ul>
+        <li>sadadasdasd</li>
+      </ul>
+    </nav>
+  </header>
+
+</template>
+
+<script>
+  export default{
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+  header {
+    height: 60px;
+    width: 100%;
+    background-color: #000;
+  }
+</style>
